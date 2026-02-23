af4e00f60c80c00baba57a79a4c031ab9ca13baa
nuribasar <nuri.basar@std.bogazici.edu.tr>
2026-02-23 11:34:42 +0300

    Step 2.14: Add CameraFollow script for smooth camera tracking

---
+++ UnityEntryProject/Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    public Transform target;
+    public Vector3 offset = new Vector3(0, 8, -12);
+
+    void LateUpdate()
+    {
+        transform.position = target.position + offset;
+        transform.LookAt(target);
+    }
+}
